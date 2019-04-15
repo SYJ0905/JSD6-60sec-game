21ad290968f959dc952e8b52f390a42e7407a5bb
CloudSu <BetaPhoenixSNSD@gmail.com>
2019-04-15 13:23:13 +0800

    create project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'no-console': 0,
+  },
+};
